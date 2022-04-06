8c4bded307b35151c775888c07e6898fe33bc915
Den <kdv2kz@yandex.ru>
2022-04-06 23:16:46 +0600

    create application

--- hw01-gradle/src/main/java/ru/otus/Application.java
+++ hw01-gradle/src/main/java/ru/otus/Application.java
@@ -1,7 +1,13 @@
 package ru.otus;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 public class Application {
     public static void main(String... args) {
+        List<String> vals = Lists.newArrayList("val01", "val02", "val03", "val04");
+        System.out.println(Lists.reverse(vals));
 
     }
 }
